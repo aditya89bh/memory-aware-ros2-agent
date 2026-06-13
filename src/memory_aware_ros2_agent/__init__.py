e98469353b28ea3557f1f3b170fdd703fe8f4141
Aditya <aditya89bh@gmail.com>
2026-06-13 10:29:06 +0530

    Add package version utilities

--- src/memory_aware_ros2_agent/__init__.py
+++ src/memory_aware_ros2_agent/__init__.py
@@ -1,5 +1,7 @@
 """Memory-aware ROS2 agent package."""
 
+from memory_aware_ros2_agent._version import __version__
+
 PACKAGE_NAME = "memory-aware-ros2-agent"
 
-__all__ = ["PACKAGE_NAME"]
+__all__ = ["PACKAGE_NAME", "__version__"]
